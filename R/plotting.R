8423554a33f3cde2b40f3ff591c5770836cc6784
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2016-06-07 20:59:35 +0200

    first commit

---
+++ R/plotting.R
@@ -0,0 +1,31 @@
+#
+# Plotting functions
+#
+
+#' Plots a discussion tree using nice parameters
+#'
+#' @param gtree igraph object
+#' @param labels type of label to be shown over vertices, if any
+#' @export
+plot.tree <- function(gtree, labels=c('name', 'id')){
+  # Plots a tree graph
+  # Arguments:
+  #   gtree: a igraph object graph with no cycles (tree)
+  if (missing(labels)){
+    labels <- NA
+  }
+  else{
+  labels <- switch(labels, 
+                   'name' = V(gtree)$name, 
+                   'id' = as.numeric(V(gtree)))
+  }
+  par(mfrow=c(1,1))
+  gtree.un <- as.undirected(gtree)
+  la = layout_as_tree(gtree.un, mode='out', root=which.min(V(gtree.un)$date))
+
+  plot(gtree.un,
+       layout = la,
+       vertex.label = labels,
+       vertex.size=3,
+       edge.arrow.size=0.6)
+}
